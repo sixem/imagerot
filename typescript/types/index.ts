ad1339c109780ed202022d45960f373d6b02a139
emy <emy@five.sh>
2023-07-05 04:33:58 +0200

    Update type exports

--- typescript/types/index.ts
+++ typescript/types/index.ts
@@ -1,12 +1,25 @@
-interface BufferHandlerParams {
+interface IBufferHandlerParams {
     data: Uint8Array;
     width: number;
     height: number;
 };
 
-type BufferHandler = (params: BufferHandlerParams) => Uint8Array;
+type TBufferHandler = (params: IBufferHandlerParams) => Uint8Array;
+
+interface IRotParams {
+    data?: Uint8Array | Object | Buffer | File;
+    url?: string;
+    dimensions?: [number, number];
+    mode?: string;
+};
+
+type TRotHandler = (params: IRotParams) => void;
+type IRotData = [Uint8Array | null, number, number];
 
 export {
-    BufferHandler,
-    BufferHandlerParams
+    TBufferHandler,
+    IBufferHandlerParams,
+    TRotHandler,
+    IRotParams,
+    IRotData
 };
