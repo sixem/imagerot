8469f178c8df0d809c9b3d12a4308e7570a7eedc
emy <emy@five.sh>
2023-07-08 00:10:58 +0200

    Update configuration to support contextualized usage of effects

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,61 @@
+const webpack = require('webpack');
 const path = require('path');
 
-module.exports = {
-    entry: './typescript/imagerot/browser/index.ts',
-    output: {
-        filename: 'imagerot.js',
+module.exports = (_, argv) => {
+    const isProduction = argv.mode === 'production';
+
+    const output = {
         path: path.resolve(__dirname, 'dist'),
         library: 'imagerot',
         libraryTarget: 'umd',
         globalObject: 'this',
         umdNamedDefine: true
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: [
-                    '/node_modules/',
-                    '/typescript/imagerot/node/**/*'
-                ]
-            },
-        ],
-    }
+    };
+
+    const config = {
+        entry: './typescript/imagerot/browser/index.ts',
+        resolve: {
+            extensions: ['.ts', '.js']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: 'ts-loader',
+                    exclude: [
+                        '/node_modules/',
+                        '/typescript/imagerot/node/**/*'
+                    ]
+                },
+            ],
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                ENVIRONMENT: JSON.stringify('browser')
+            })
+        ]
+    };
+
+    const devConfig = {
+        ...config,
+        mode: 'development',
+        optimization: {
+            minimize: false
+        },
+        output: {
+            filename: 'imagerot.js',
+            ...output
+        },
+    };
+
+    const prodConfig = {
+        ...config,
+        mode: 'production',
+        output: {
+            filename: 'imagerot.min.js',
+            ...output
+        },
+    };
+
+    return isProduction ? [devConfig, prodConfig] : devConfig;
 };
