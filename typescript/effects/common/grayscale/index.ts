94b99a735df1a686a36f4c2a57fa6ca6ed4c73cc
emy <emy@five.sh>
2023-07-08 23:11:08 +0200

    Clean up effect

--- typescript/effects/common/grayscale/index.ts
+++ typescript/effects/common/grayscale/index.ts
@@ -6,21 +6,20 @@ type TEffectOptions = {
 
 const defaultIntensity = 1;
 
-const global: TEffectItem = async ({ data, width, height }, options = null) => {
+const global: TEffectItem = async ({ data }, options = null) => {
     const { intensity = defaultIntensity } = (options || {}) as TEffectOptions;
 
     let grayData = new Uint8Array(data);
 
     for (let i = 0; i < data.length; i += 4) {
         let gray = intensity * (0.299 * Number(data[i]) + 0.587 * Number(data[i+1]) + 0.114 * Number(data[i+2]));
-        gray = Math.min(255, gray); // Ensure the value doesn't exceed 255
-        grayData[i] = grayData[i+1] = grayData[i+2] = gray;
-        grayData[i+3] = data[i+3];
+        gray = Math.min(255, gray);
+        grayData[i] = grayData[i + 1] = grayData[i + 2] = gray;
+        grayData[i + 3] = data[i + 3];
     }
 
     return grayData;
 };
-
 
 const grayscale: TEffectExport = {
     name: 'grayscale',
